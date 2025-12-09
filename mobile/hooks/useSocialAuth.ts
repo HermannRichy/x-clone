28ff551eeb521778cc56871a79db6110025d68c7
Hermann Richy <hermannrichy15@gmail.com>
2025-12-09 17:53:19 +0100

    auth implemented

---
+++ mobile/hooks/useSocialAuth.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useSSO } from "@clerk/clerk-expo";
+import { Alert } from "react-native";
+
+export const useSocialAuth = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const { startSSOFlow } = useSSO();
+    const handleSocialAuth = async (
+        strategy: "oauth_google" | "oauth_apple"
+    ) => {
+        setIsLoading(true);
+
+        try {
+            const { createdSessionId, setActive } = await startSSOFlow({
+                strategy,
+            });
+            if (createdSessionId && setActive) {
+                await setActive({ session: createdSessionId });
+            }
+        } catch (err) {
+            console.log("Social auth error:", err);
+            const provider = strategy === "oauth_google" ? "Google" : "Apple";
+            Alert.alert(
+                "Erreur",
+                `Échec de la connexion avec ${provider}. Veuillez réessayer.`
+            );
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    return { isLoading, handleSocialAuth };
+};
